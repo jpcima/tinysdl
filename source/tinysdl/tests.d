5eeb9b2d4804152c90d349537371eec822f27c48
JP Cimalando <jp-dev@inbox.ru>
2016-05-15 11:17:08 +0200

    add more tests

--- source/tinysdl/tests.d
+++ source/tinysdl/tests.d
@@ -1,6 +1,9 @@
 module tinysdl.tests;
 import tinysdl;
-import std.array: empty;
+import std.stdio;
+import std.array: array, empty, join;
+import std.string: replace, splitLines;
+import std.algorithm: map, endsWith, stripRight;
 
 unittest {
   // empty document
@@ -11,7 +14,60 @@ unittest {
   }
 
   // example document
-  { Tag doc = parse(`
+  { Tag doc = parse(exampleDocument);
+    validateExampleDocument(doc);
+  }
+
+  { enum withoutIndentation = exampleDocument.replace("\t", "");
+    Tag doc = parse(withoutIndentation);
+    validateExampleDocument(doc);
+  }
+
+  { enum withSpaceIndentation = exampleDocument.replace("\t", " ");
+    Tag doc = parse(withSpaceIndentation);
+    validateExampleDocument(doc);
+  }
+
+  { enum withTrailingSpaces = exampleDocument.replace("\n", " \n");
+    Tag doc = parse(withTrailingSpaces);
+    validateExampleDocument(doc);
+  }
+
+  { enum withTrailingTabs = exampleDocument.replace("\n", "\t\n");
+    Tag doc = parse(withTrailingTabs);
+    validateExampleDocument(doc);
+  }
+
+  { enum withCRs = exampleDocument.replace("\n", "\r\n");
+    Tag doc = parse(withCRs);
+    validateExampleDocument(doc);
+  }
+
+  { enum withEmptyLines = "\n" ~ exampleDocument.replace("\n", "\n\n");
+    Tag doc = parse(withEmptyLines);
+    validateExampleDocument(doc);
+  }
+
+  { enum withoutFinalNewline = exampleDocument.stripRight('\n');
+    Tag doc = parse(withoutFinalNewline);
+    validateExampleDocument(doc);
+  }
+
+  { enum withSemicolons =
+        exampleDocument
+        .splitLines()
+        .map!((a) =>
+              a.empty() ? "" :
+              a.endsWith('{') ? (a ~ '\n') :
+              a.endsWith('}') ? ('\n' ~ a) :
+              a ~ ';')
+        .join("");
+    Tag doc = parse(withSemicolons);
+    validateExampleDocument(doc);
+  }
+}
+
+enum exampleDocument = `
 	"This is an anonymous tag with two values" 123
 	"Another anon tag"
 	person "Akiko" "Johnson" dimensions:height=68 {
@@ -23,74 +79,75 @@ unittest {
 			smoker false
 		}
 	}
-`);
-    assert(doc.children.length == 3);
+`;
 
-    assert(doc.children[0].name == "");
-    assert(doc.children[0].values.length == 2);
-    assert(doc.children[0].values[0].asText == "This is an anonymous tag with two values");
-    assert(doc.children[0].values[1].asNumber == 123);
-    assert(doc.children[0].attributes.empty);
-    assert(doc.children[0].children.empty);
+void validateExampleDocument(Tag doc) {
+  assert(doc.children.length == 3);
 
-    assert(doc.children[1].name == "");
-    assert(doc.children[1].values.length == 1);
-    assert(doc.children[1].values[0].asText == "Another anon tag");
-    assert(doc.children[1].attributes.empty);
-    assert(doc.children[1].children.empty);
+  assert(doc.children[0].name == "");
+  assert(doc.children[0].values.length == 2);
+  assert(doc.children[0].values[0].asText == "This is an anonymous tag with two values");
+  assert(doc.children[0].values[1].asNumber == 123);
+  assert(doc.children[0].attributes.empty);
+  assert(doc.children[0].children.empty);
 
-    doc = doc.children[2];
-    assert(doc.name == "person");
-    assert(doc.values.length == 2);
-    assert(doc.values[0].asText == "Akiko");
-    assert(doc.values[1].asText == "Johnson");
-    assert(doc.values.length == 2);
-    assert(doc.attributes.length == 1);
-    assert(doc.attributes[0].name == "dimensions:height");
-    assert(doc.attributes[0].value.asNumber == 68);
-    assert(doc.children.length == 3);
+  assert(doc.children[1].name == "");
+  assert(doc.children[1].values.length == 1);
+  assert(doc.children[1].values[0].asText == "Another anon tag");
+  assert(doc.children[1].attributes.empty);
+  assert(doc.children[1].children.empty);
 
-    assert(doc.children[0].name == "son");
-    assert(doc.children[0].values.length == 2);
-    assert(doc.children[0].values[0].asText == "Nouhiro");
-    assert(doc.children[0].values[1].asText == "Johnson");
-    assert(doc.children[0].attributes.empty);
-    assert(doc.children[0].children.empty);
+  doc = doc.children[2];
+  assert(doc.name == "person");
+  assert(doc.values.length == 2);
+  assert(doc.values[0].asText == "Akiko");
+  assert(doc.values[1].asText == "Johnson");
+  assert(doc.values.length == 2);
+  assert(doc.attributes.length == 1);
+  assert(doc.attributes[0].name == "dimensions:height");
+  assert(doc.attributes[0].value.asNumber == 68);
+  assert(doc.children.length == 3);
 
-    assert(doc.children[1].name == "pet:kitty");
-    assert(doc.children[1].values.length == 1);
-    assert(doc.children[1].values[0].asText == "Neko");
-    assert(doc.children[1].attributes.empty);
-    assert(doc.children[1].children.empty);
+  assert(doc.children[0].name == "son");
+  assert(doc.children[0].values.length == 2);
+  assert(doc.children[0].values[0].asText == "Nouhiro");
+  assert(doc.children[0].values[1].asText == "Johnson");
+  assert(doc.children[0].attributes.empty);
+  assert(doc.children[0].children.empty);
 
-    doc = doc.children[2];
-    assert(doc.name == "daughter");
-    assert(doc.values.length == 2);
-    assert(doc.values[0].asText == "Sabrina");
-    assert(doc.values[1].asText == "Johnson");
-    assert(doc.attributes.length == 1);
-    assert(doc.attributes[0].name == "location");
-    assert(doc.attributes[0].value.asText == "Italy");
-    assert(doc.children.length == 3);
+  assert(doc.children[1].name == "pet:kitty");
+  assert(doc.children[1].values.length == 1);
+  assert(doc.children[1].values[0].asText == "Neko");
+  assert(doc.children[1].attributes.empty);
+  assert(doc.children[1].children.empty);
 
-    assert(doc.children[0].name == "hobbies");
-    assert(doc.children[0].values.length == 2);
-    assert(doc.children[0].values[0].asText == "swimming");
-    assert(doc.children[0].values[1].asText == "surfing");
-    assert(doc.children[0].attributes.empty);
-    assert(doc.children[0].children.empty);
+  doc = doc.children[2];
+  assert(doc.name == "daughter");
+  assert(doc.values.length == 2);
+  assert(doc.values[0].asText == "Sabrina");
+  assert(doc.values[1].asText == "Johnson");
+  assert(doc.attributes.length == 1);
+  assert(doc.attributes[0].name == "location");
+  assert(doc.attributes[0].value.asText == "Italy");
+  assert(doc.children.length == 3);
 
-    assert(doc.children[1].name == "languages");
-    assert(doc.children[1].values.length == 2);
-    assert(doc.children[1].values[0].asText == "English");
-    assert(doc.children[1].values[1].asText == "Italian");
-    assert(doc.children[1].attributes.empty);
-    assert(doc.children[1].children.empty);
+  assert(doc.children[0].name == "hobbies");
+  assert(doc.children[0].values.length == 2);
+  assert(doc.children[0].values[0].asText == "swimming");
+  assert(doc.children[0].values[1].asText == "surfing");
+  assert(doc.children[0].attributes.empty);
+  assert(doc.children[0].children.empty);
 
-    assert(doc.children[2].name == "smoker");
-    assert(doc.children[2].values.length == 1);
-    assert(doc.children[2].values[0].asBoolean == false);
-    assert(doc.children[2].attributes.empty);
-    assert(doc.children[2].children.empty);
-  }
+  assert(doc.children[1].name == "languages");
+  assert(doc.children[1].values.length == 2);
+  assert(doc.children[1].values[0].asText == "English");
+  assert(doc.children[1].values[1].asText == "Italian");
+  assert(doc.children[1].attributes.empty);
+  assert(doc.children[1].children.empty);
+
+  assert(doc.children[2].name == "smoker");
+  assert(doc.children[2].values.length == 1);
+  assert(doc.children[2].values[0].asBoolean == false);
+  assert(doc.children[2].attributes.empty);
+  assert(doc.children[2].children.empty);
 }
